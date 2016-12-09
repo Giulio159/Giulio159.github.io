5ee47fc1c2af5152b2a0926052447d4632bd36af
Giulio159 <giulioantonio.gagliardi@gmail.com>
2016-12-09 12:52:53 -0800

    Update Entities.js

--- js/Entities.js
+++ js/Entities.js
@@ -366,7 +366,7 @@ Upgrade.update = function(){
 		if(isColliding){
 			if(Upgrade.list[key].category === 'score'){
 				score += 500;
-               			player.hp = 10;
+               			player.hp += (10 - player.hp);
             delete Upgrade.list[key];
             }
 			if(Upgrade.list[key].category === 'atkSpd'){
